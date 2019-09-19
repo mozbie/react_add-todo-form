b45c6cec3be855f82fd7ff5af6960e43d44f7bbd
Alina Kizilova <alinakzilova@gmail.com>
2019-09-20 00:29:59 +0300

    Updates

---
+++ precache-manifest.bffcc2c820718225e37dbf8d0da5e416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084cc695257703dd6848fe69e879ffd2",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "e2281f6eef30f824f4df",
+    "url": "/react_add-todo-form/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "8a40ffc36ffb7aad6a63",
+    "url": "/react_add-todo-form/static/css/main.acd236e1.chunk.css"
+  },
+  {
+    "revision": "e2281f6eef30f824f4df",
+    "url": "/react_add-todo-form/static/js/2.9eb38649.chunk.js"
+  },
+  {
+    "revision": "8a40ffc36ffb7aad6a63",
+    "url": "/react_add-todo-form/static/js/main.8becc7cd.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
